bef7e341aae6c1b86881e166d305f2257b6f0ff3
Jacob Smith <jsmith0361@jh-mba158-19.local>
2019-04-05 11:43:22 -0500

    added zoom, addedBarButtonZoom, sets currentLocation, span, region, center, pizza[ ]

--- Za Hunter/ViewController.swift
+++ Za Hunter/ViewController.swift
@@ -7,14 +7,67 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
+    @IBOutlet weak var mapView: MKMapView!
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
+    var pizza: [MKMapItem] = []
+    
+    var initialRegion: MKCoordinateRegion!
+    var isInitialRegion = true
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        mapView.delegate = self
+        
+    }
+    
+    func mapViewDidFinishLoadingMap(_ mapView: MKMapView) {
+        if isInitialRegion {
+            initialRegion = MKCoordinateRegion(center: mapView.centerCoordinate, span: mapView.region.span)
+            isInitialRegion = false
+        }
     }
 
-
+    @IBAction func whenPizzaSearchButtonPressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Pizza"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error) in
+            guard let response = response else { return }
+            for mapItem in response.mapItems {
+                self.pizza.append(mapItem)
+                let annotation = MKPointAnnotation()
+                
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
+    }
+    
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+    }
 }
 
